7273614da3128d2ee65416f45718a3c87ffede67
anthony.enomoto <anthony.enomoto@gmail.com>
2026-05-18 20:38:03 +0900

    Added streamlit map to project.

--- Main.py
+++ Main.py
@@ -3,3 +3,4 @@ import streamlit as st
 st.title("Hello World")
 st.subheader("This is a subheader example")
 st.markdown("**Hello**, *Streamlit! This is a markdown example.*")
+st.map(data=None, use_container_width=True, latitude=37.76, longitude=-122.4)
